ba5435aac3b2b1615f95a5dabd28945ddcf6f52b
Chien0127 <erica12793@gmail.com>
2024-04-19 14:29:58 +0800

    0419

---
+++ Cf5/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Cf5"
+include(":app")
+ 
